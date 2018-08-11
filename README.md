8709b210cedc52aee4c37cda8bd1c7ee3fcacf81
Mantilas <lexa1th@gmail.com>
2018-08-11 22:53:00 +0800

    Initial project

---
+++ README.md
@@ -0,0 +1,25 @@
+# Template0.1.1
+Configured packages such as npm, bower, sass, gulp, and more.
+
+## Install
+1. git clone https://github.com/AlexsofarBOT/Template0.1.1.git
+2. npm i
+3. bower i
+4. gulp
+
+## Build the project in the dist folder
+* gulp build
+
+## Installed plugins:
+* gulp
+* gulp-autoprefixer
+* gulp-clean
+* gulp-if
+* gulp-minify
+* gulp-responsive
+* gulp-sass
+* gulp-sftp
+* gulp-uglify
+* gulp-uncss
+* gulp-useref
+* wiredep
---
+++ app/js/products.js
@@ -0,0 +1,21 @@
+$(function() {
+   $.getJSON('data/products.json', function(data)
+   {
+      var output = "";
+      for(var i in data.products)
+      {
+         output +=
+            "<div class=card-row>"
+            + "<div class=card-image>"
+               + "<img src=" + data.products[i].image + " alt=error>"
+            + "</div>"
+            + "<div class=card-text>"
+               + "<div class=card-element>" + data.products[i].title + "</div>"
+               + "<div class=card-element>" + data.products[i].description + "</div>"
+            + "</div>"
+          + "</div>";
+      }
+       console.log("it works!");
+      $(".card-container").append(output);
+   });
+});
